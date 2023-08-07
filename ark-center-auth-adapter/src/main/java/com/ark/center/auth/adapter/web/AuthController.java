f79be4644dc835cbd03d430f54114ede23c5dae6
Javis <javischen9548@foxmail.com>
2023-08-07 18:00:22 +0800

    u

--- ark-center-auth-adapter/src/main/java/com/ark/center/auth/adapter/web/AuthController.java
+++ ark-center-auth-adapter/src/main/java/com/ark/center/auth/adapter/web/AuthController.java
@@ -1,11 +1,27 @@
 package com.ark.center.auth.adapter.web;
 
+import com.ark.component.web.base.BaseController;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AuthController {
+public class AuthController extends BaseController {
+
+    public static void main(String[] args) {
+        String id = convertToUUID("F1C539DC61424F7A9F2CB3EF9E73C93B");
+        System.out.println(id);
+    }
+
+    private static String convertToUUID(String uuidStr) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(uuidStr, 0, 8).append("-")
+                .append(uuidStr, 8, 12).append("-")
+                .append(uuidStr, 12, 16).append("-")
+                .append(uuidStr, 16, 20).append("-")
+                .append(uuidStr.substring(20));
+        return stringBuffer.toString();
+    }
 
     @GetMapping("/admin/test")
 //    @PreAuthorize("hasRole('ROLE_S')")
